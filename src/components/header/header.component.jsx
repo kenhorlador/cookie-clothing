50258e1bc8a9bee20913b892f7c18ddd5776f4a4
Ken Barrameda Horlador <kenhorlador@gmail.com>
2021-11-11 23:00:07 +0800

    added header component and linked shop page

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,29 @@
+// Styles
+import './header.styles.css'
+
+// Assets
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+// React Router Dom
+import { Link } from 'react-router-dom'
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className='options'>
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  ) 
+}
+
+export default Header
